b8ab1b33025683c1b0e7fe9792c92130a422baad
Steve Elliott <steve@tegud.net>
2020-12-19 07:26:38 +0000

    pick up write to file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-exec krane $@
+IFS=' ' read -r -a array <<< "$@"
+
+if [ "${array[-2]}" = ">" ]
+then
+  exec krane "${array[@]::${#array[@]}-2}" > ${array[-1]}
+else
+  exec krane $@
+fi
+
